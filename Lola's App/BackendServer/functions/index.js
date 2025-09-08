961a270777c363a5d0b46308bbc10b0d85f9f9c2
oaobielodan <oaobielodan@uchicago.edu>
2025-09-08 01:21:33 -0400

    cleaned up code

--- Lola's App/BackendServer/functions/index.js
+++ Lola's App/BackendServer/functions/index.js
@@ -25,7 +25,8 @@ exports.addPlaylist = onRequest(async (req, res) => {
 // Listens for new messages added to /playlists/:documentId/genre
 // and saves a playlist associated with that genre and the cook time
 // to /playlists/:documentId/playlist
-exports.makePlaylist = onDocumentCreated("/playlists/{documentId}", (event) => {
+exports.makePlaylist =
+onDocumentCreated("/playlists/{documentId}", async (event) => {
   // grab the current value of what was written to Firestore.
   const genre = event.data.data().genre;
   const length = event.data.data().length;
@@ -34,10 +35,48 @@ exports.makePlaylist = onDocumentCreated("/playlists/{documentId}", (event) => {
   logger.log("making playlist", event.params.documentId, genre, length);
 
   // THIS IS WHERE WE MAKE PLAYLIST
-  const playlist = `${genre} and ${length}`;
+  const recommendations = await getSpotifyPlaylist(genre, length);
 
   // you must return a Promise when performing asynchronous tasks inside a
   // function such as writing to Firestore.
   // setting an 'playlist' field in Firestore document returns a Promise.
-  return event.data.ref.set({playlist}, {merge: true});
+  return recommendations;
 });
+
+const getSpotifyAccessToken = async () => {
+  const clientId = logger.config().spotify.client_id;
+  const clientSecret = logger.config().spotify.client_secret;
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": "Basic " + btoa( clientId + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  return data.access_token;
+};
+
+// helper to get recommendations
+const getRecommendations = async (token, genre, limit = 10) => {
+  const result = await fetch(`https://api.spotify.com/v1/recommendations?seed_genres=${genre}&limit=${limit}`, {
+    headers: {
+      "Authorization": `Bearer ${token}`,
+    },
+  });
+
+  return await result.json();
+};
+
+const getSpotifyPlaylist = async (genre, length) => {
+  // this math is done under the assumption that the average song is about
+  // 3 mins long (length is in mins) => 10 extra songs are added for variabiity
+  const limit = (length / 3) + 10;
+  const accessToekn = getSpotifyAccessToken();
+  const recommendations = await getRecommendations(accessToekn, genre, limit);
+
+  return recommendations;
+};
